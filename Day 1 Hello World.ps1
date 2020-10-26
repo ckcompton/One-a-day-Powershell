5df1de21a463d5425fd3d23346745b4461096c36
ckcompton <ckcompton@yahoo.com>
2020-10-26 11:05:35 -0500

    Day 1

---
+++ Day 1 Hello World.ps1
@@ -0,0 +1,6 @@
+# This is a comment
+# I will be using Powershell Core 7 instead of powershell
+# The difference between the two versions can be found here https://www.ghacks.net/2018/01/12/powershell-vs-powershell-core-what-you-need-to-know/
+
+ 
+Write-Host "Hello World!"
