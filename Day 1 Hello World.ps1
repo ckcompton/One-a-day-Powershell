749aecb4e5fd3792c5eb0fac78451093b76ced15
Cameron Compton <ckcompton@yahoo.com>
2020-10-26 20:56:01 -0500

    master

--- Day 1 Hello World.ps1
+++ Day 1 Hello World.ps1
@@ -1,6 +1,8 @@
 # This is a comment
 # I will be using Powershell Core 7 instead of powershell
 # The difference between the two versions can be found here https://www.ghacks.net/2018/01/12/powershell-vs-powershell-core-what-you-need-to-know/
+# I am using Visual Studio Code and the Powershell extension
+# I belive Visual Studio Code has github functionality built in
 
  
 Write-Host "Hello World!"
